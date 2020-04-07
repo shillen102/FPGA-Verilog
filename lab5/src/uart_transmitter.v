9cf9d108b7a12aaeb0aecbf0d7a023ac9a92da86
shillen102 <atp002@berkeley.edu>
2020-04-07 07:36:54 -0700

    uart_transmitter

--- lab5/src/uart_transmitter.v
+++ lab5/src/uart_transmitter.v
@@ -15,37 +15,43 @@ module uart_transmitter #(
     localparam  SYMBOL_EDGE_TIME    =   CLOCK_FREQ / BAUD_RATE;
     localparam  CLOCK_COUNTER_WIDTH =   $clog2(SYMBOL_EDGE_TIME);
 
-    // Remove these assignments when implementing this module
-    // assign serial_out = 1'b0;
-    // assign data_in_ready = 1'b0;
-
     reg [CLOCK_COUNTER_WIDTH-1:0] clock_counter;
 
-    assign symbol_edge = clock_counter == (SYMBOL_EDGE_TIME - 1);
-
-    //--|Counters|----------------------------------------------------------------
-
-    // Counts cycles until a single symbol is done
-    always @(posedge clk) begin
-        clock_counter <= (start || reset || symbol_edge) ? 0 : clock_counter + 1;
-    end
-
-    reg data_out;
-    reg [3:0] bit_counter;
+    wire symbol_edge;
     wire tx_running;
-    
-    assign tx_running = bit_counter != 4'b0;
-    /// do: start, 
 
-    // Count down from 10 bits for every character
-    always @(posedge clk) begin
-        if (reset) begin
-            bit_counter <= 0;
-        end else if (start) begin
-            bit_counter <= 10;
-        end else if (symbol_edge && tx_running) begin
-            bit_counter <= bit_counter - 1;
-        end
+    reg [3:0] bit_counter;
+    reg out;
+
+    assign symbol_edge = clock_counter == (SYMBOL_EDGE_TIME - 1);
+    assign tx_running = bit_counter != 4'd0;
+
+    always@(posedge clk) begin
+        clock_counter <= (clock_counter == SYMBOL_EDGE_TIME) ? 0: clock_counter + 1;
     end
+    
+    always@(posedge clk) begin
+        if(reset)
+            bit_counter <= 0;
+        else if (data_in_valid)
+            bit_counter <= 4'd10;
+        else if (symbol_edge && tx_running)
+            bit_counter <= bit_counter - 4'd1;
+    end
+    
+    always@(*) begin
+        if (bit_counter == 4'd10)
+            out = 0;
+        else if (bit_counter <= 4'd1)
+            out = 1;
+        else
+            out = data_in[bit_counter - 1];
+    end
+
+    //--|Signal Assignments|------------------------------------------------------
+    assign serial_out = out;
+    assign data_in_ready = tx_running;
+
+
 
 endmodule
